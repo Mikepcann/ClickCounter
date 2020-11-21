69cdb23416cfc10ba374ff2989f245b429d37b9a
Mikepcann <mikepcannbass@gmail.com>
2020-11-21 15:47:32 -0700

    Adding a button to change the value that the counter moves

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A simple click counter using JS
 
 Todo
-1) add a reset button
+1) add a reset button - COMPLETE
 2) Add logic to not let the counter go below 0, and display a message to the user that a number below 0 isn't allowed
 3) Build a toggle button to "allow numbers below 0, deny numbers below 0"
 4) Build a drop down that allows the user to increment the counter by 1, 2, or 3 digits at a time
--- clickcounter.js
+++ clickcounter.js
@@ -1,6 +1,7 @@
 //declaring the staring point of the count
 let x = 0;
- 
+
+
 document.getElementById('counter').innerHTML= x;
 
 function countMover(method){
@@ -18,7 +19,13 @@ function countMover(method){
         console.log(x);
     }
 };
-        
+  
+// Change value of the counter function Def
+function changeValue(){
+    console.log("button click, and the change value is ran");
+    return document.getElementById("value").value;
+    
+}
 //connecting the downCount() to the decrease button
 document.getElementById("decrease").onclick=function(){
     countMover("subtract"); 
@@ -30,5 +37,11 @@ document.getElementById("increase").onclick=function(){
 //reset Count of Button
 document.getElementById("reset").onclick=function(){
     countMover();
+}
+//Change the value of the count with tryit button
+document.getElementById("clickedButt").onclick=function(){
+    changeValue();
+    console.log(document.getElementById("value").value)
+
 }
 
